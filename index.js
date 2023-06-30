f3f5767fd8bf10f97fa02c25560c3e1f385c1e70
outaagunga <jeconiaagunga@gmail.com>
2023-06-30 14:55:04 +0300

    Completed the test

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+window.customerName = 'bob';
+
+function getCustomerName() {
+  return customerName;
+}
+// Converting customerName to upper case
+function upperCaseCustomerName() {
+    window.customerName = window.customerName.toUpperCase();
+  }
+  //Setting the best customer
+  function setBestCustomer() {
+    window.bestCustomer = 'not bob';
+  }
+  
+  //Overwriting the best customer
+  function overwriteBestCustomer(customer) {
+    window.bestCustomer = customer;
+  }
+  //Changing the leastFavoriteCustomer
+  function changeLeastFavoriteCustomer() {
+    throw new Error('Assignment to constant variable.');
+  }
+  
